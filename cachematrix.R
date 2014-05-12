8f38bcf593dbe03e75318750186e52ec3391b8af
Ted Ahern <tedahern@hotmail.com>
2014-05-12 12:47:04 +0300

    Update
    Updated assignment

--- cachematrix.R
+++ cachematrix.R
@@ -30,7 +30,7 @@ cacheSolve <- function(x, ...) {
                 return(inv)
         }
         data <- x$get()
-        inv <- inverse(data, ...)
+        inv <- solve(data, ...)
         x$setinverse(inv)
         inv
 }
